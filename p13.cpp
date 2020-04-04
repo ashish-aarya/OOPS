ed0e629bca849c27bad8d57d20159be7e8170eb8
Ashish Arya <arya.ashish229@gmail.com>
2020-04-04 13:11:43 +0530

    Add files via upload

---
+++ p13.cpp
@@ -0,0 +1,47 @@
+#include <iostream.h>
+#include <stdio.h>
+#include <conio.h>
+struct name 
+{
+	char[40] First;
+	char[40] Mid;
+	char[40] Last;
+};
+struct phone 
+{
+	char[4] Area;
+	char[4] Exch;
+	char[6] Numb;
+};
+name n;
+phone p;
+class p_rec
+{
+	p_rec()
+	{
+
+	}
+	void input ()
+	{
+		gets(n.First);
+		gets(n.Mid);
+		gets(n.Last);	
+		gets(p.Area);
+		gets(p.Exch);
+		gets(p.Numb);
+	}
+	void display ()
+	{
+		cout<<n.First;
+		cout<<n.Mid;
+		cout<<n.Last;	
+		cout<<p.Area;
+		cout<<p.Exch;
+		cout<<p.Numb;
+	}
+};
+void main ()
+{ p_rec p=new p_rec();
+	p.input();
+	p.display();   
+}
